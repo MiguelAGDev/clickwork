fa51206afb165f1e2b4f7224adc8e067aba5a1b0
MiguelAGDev <miguelangel.dev@hotmail.com>
2026-05-08 23:33:48 -0600

    FIX: erroHandler.js add support for statusCode

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -16,7 +16,7 @@
  */
 export async function errorHandler(err, req, res, next) {
 
-    const status = err.status || 500;
+    const status =  err.statusCode || err.status || 500;
     const message = err.message || 'Internal Server Error';
 
     if(process.env.NODE_ENV == 'development'){
